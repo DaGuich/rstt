97abdd80d3d167caedb1256d342c30a7a27cf747
Matthias Gilch <matthias.gilch.mg@gmail.com>
2020-09-04 21:03:53 +0200

    Documented some modules

--- src/handler/client.rs
+++ src/handler/client.rs
@@ -1,13 +1,31 @@
+use std::collections::HashMap;
 use std::net::TcpStream;
 
 use anyhow::{anyhow, Result};
 
+enum ClientState {
+    /// The client is not connected to the server
+    New,
+    // The client has sent the connection packet but is not acknowledged yet
+    // Connected,
+
+    // The clients connection is acknowledged
+    // ConnAcked,
+}
+
+/// The MQTT client
 pub struct MqttClient {
+    /// The tcp stream to the server
+    ///
+    /// TODO: check if this stream has to be reopened... or can be reopened
     stream: TcpStream,
 }
 
 impl MqttClient {
-    pub fn new(constr: &str) -> Result<MqttClient> {
+    /// Create a new client instance
+    ///
+    /// This method does not connect to the server
+    pub fn new(constr: &str, options: HashMap<&str, &str>) -> Result<MqttClient> {
         let stream = match TcpStream::connect(constr) {
             Ok(s) => s,
             Err(e) => {
@@ -16,4 +34,7 @@ impl MqttClient {
         };
         Ok(MqttClient { stream })
     }
+
+    /// Connect to the server.
+    pub fn connect(self: &mut Self) {}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,6 @@ mod handler;
 mod packet;
 mod util;
 
-pub use packet::deserialize;
-pub use packet::serialize;
+pub use handler::client::MqttClient;
 pub use packet::PType as PacketType;
+pub use packet::{deserialize, serialize};
